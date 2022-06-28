529b5f9ee19e84eb85d813cd09bf6b2fdf209216
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2022-06-28 20:34:46 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-console.log("Feel free to write \n es6 code in this file");
+import { test } from "./server/main";
+
+console.log("Feel free to write \n es6 code in this file \t");
+test("imported function, called");
